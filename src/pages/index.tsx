3dc184b23071225074619d610612327a3ccc0321
Isabellagouveias <isabellagouveias@gmail.com>
2023-08-23 14:55:10 -0300

    feat: button e animação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
+import Head from "next/head";
+import logoImg from "../../public/logo.svg";
+import styles from "../../styles/Home.module.scss";
+import Image from "next/image";
+import { Input } from "../components/ui/Input";
+import { Button } from "../components/ui/Button";
 export default function Home() {
   return (
-    <div>
-      <h1>Sujeito Pizza :)</h1>
-    </div>
+    <>
+      <Head>
+        <title>SujeitoPizza - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenteer}>
+        <Image src={logoImg} alt="Logo Sujeito Pizarria" />
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu e-mail" type="text" />
+            <Input placeholder="Digite sua senha" type="password" />
+            <Button type="submit" loading={true}>
+              Cadastrar
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
